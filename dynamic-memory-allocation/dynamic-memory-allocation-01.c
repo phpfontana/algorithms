778574d22d47a65d1bdf5d1a1b4f84bdfa54a1aa
Office <office@Offices-iMac.local>
2022-11-28 18:51:38 -0300

    update: dynamic-memory-allocation-01.c modified

--- dynamic-memory-allocation/dynamic-memory-allocation-01.c
+++ dynamic-memory-allocation/dynamic-memory-allocation-01.c
@@ -5,6 +5,7 @@
 */
 
 #include<stdio.h>
+#include<stdlib.h>
 
 // 01 Read Array
 void read_array(int *array, int n)
@@ -19,17 +20,59 @@ void read_array(int *array, int n)
 // 02 Print Array
 void print_array(int *array, int n)
 {
+    printf("Array\n");
     for (int i = 0; i < n; i++)
     {
         printf(" %d",array[i]);
     }
 }
 
+// 03 Mean of the Array
+float array_mean(int *array, int n)
+{
+    float mean = 0.0f;
+    for(int i = 0; i < n; i++)
+    {
+        mean += array[i];
+    }
+
+    mean = mean/n;
+    return mean;
+}
+
+// Memory Allocation
+int* array_memory_aloc(int n)
+{
+    int *array = (int*)malloc(n*sizeof(int));
+    if (array == NULL)
+    {
+        printf("Error!");
+    }
+
+    return array;
+}
+
 
 int main()
 {
     int *array, n;
+    float mean;
 
     printf("Input array size: ");
     scanf("%d%*c",&n);
+
+    array = array_memory_aloc(n);
+    if(array == NULL)
+        return -1;
+
+    read_array(array,n);
+    
+    print_array(array,n);
+    
+    mean = array_mean(array,n);
+    
+    printf("\nMean: %0.2f", mean);
+    free(array);
+
+    return 0;
 }
